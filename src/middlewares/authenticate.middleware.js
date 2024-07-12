c0124abcc974648a55736d7ce5e8075771923459
eperezr <eddyperez007@hotmail.com>
2024-07-12 14:25:17 -0400

    Update authenticate.middleware.js

--- src/middlewares/authenticate.middleware.js
+++ src/middlewares/authenticate.middleware.js
@@ -1,17 +1,23 @@
-import jwt from 'jsonwebtoken'
-import 'dotenv/config'
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
 
 export function authenticateToken(req, res, next){
-    const authHeader = req.headers ['authorization'];
+    //obtener token de cabezera de autorizacion
+    const authHeader = req.headers['authorization'];
+    // Bearer
+    const token = authHeader && authHeader.split('')[1];
 
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (token === null) return res.sendStatus(401);
+    if (token === null)
+        return res.sendStatus(401);
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) =>{
-        if(err) return res.sendStatus(403);
+        if (err) 
+            return res.sendStatus(403);
 
-        console.log('user', user);
-        next();
-    });
+    console.log('user',user);
+    req.user = user;
+    next();
+   });
+
 }
