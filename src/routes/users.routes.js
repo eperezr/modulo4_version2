c8606e4a86af9e42449259c850715288c7bd1bb3
eperezr <eddyperez007@hotmail.com>
2024-07-12 14:40:29 -0400

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,24 @@
-import { Router } from "express";
-import usersController from "../controllers/users.controller.js";
-import { authenticateToken } from "../middlewares/authenticate.middleware.js";
+import { Router } from 'express';
+import usersController from '../controllers/users.controller.js';
 
 const router = Router();
 
-router.route('/')
+router
+    .route('/')
+    .get(usersController.getUsers)
+    .post(usersController.createUser);
+
+router
+    .route('/:id')
+    .get(usersController.getUser)
+    .put(usersController.updateUser)
+    .delete(usersController.deleteUser)
+    .patch(usersController.activeInactive);
+
+
+export default router;
+
+/*router.route('/')
 .get(usersController.getUsers)
 .post(usersController.createUser);
 
@@ -17,4 +31,4 @@ router
 
 router.get('/:id/tasks', authenticateToken, usersController.getTasks);
 
-export default router;
+export default router;*/
