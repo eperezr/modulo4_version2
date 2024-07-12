49ab249286ae2cff1179ac205ddcb68057cf29cc
eperezr <eddyperez007@hotmail.com>
2024-07-12 14:25:39 -0400

    Update task.js

--- src/models/task.js
+++ src/models/task.js
@@ -1,24 +1,29 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../database/database.js";
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database.js';
 
-export const Task = sequelize.define('task',{
+export const Task = sequelize.define('tasks', {
+
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoincrement: true,
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
     },
+
     name:{
         type: DataTypes.STRING,
         allowNull: false,
-        validate:{
-            notNull:{
-                msg: 'ingrese tarea',
-            },
-        },
+        validate:   {
+                    notNull: {
+                        msg: 'ingrese tarea',
+                            },
+                },
     },
+    
     done: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
     },
 });
+
+
 
